4c2d649c1551f9188996eab31d04d1bb7b7cdcc1
MOHAMED RASITH <151750969+rasithirsath@users.noreply.github.com>
2025-06-23 20:31:43 +0530

    Add files via upload

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for ecommerce project.
+"""ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+admin.site.site_header = "ECOMMERCE"
+admin.site.site_title = "ARK ADMIN"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-      path('', include('store.urls')),
-]
+    path('',include('ecomapp.urls')),
+
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
